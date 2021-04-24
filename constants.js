4734d9d92283e1ea21f50f2f9858caaf967e4bc5
YskIi <iijix1141@gmail.com>
2021-04-24 23:27:34 +0900

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
-const API_KEY = 'API キーを入力してください';
-const SHEET_ID = 'Google スプレッドシートの Sheet ID を入力してください';
+const API_KEY = 'AIzaSyCGZImKMuiMaA2y5Wo_5j7tarSy630j3KU';
+const SHEET_ID = '1i96BWMvmt1vewKpHAlkEbA2GT0MdB2QoZ2Gha0oD7NE';
